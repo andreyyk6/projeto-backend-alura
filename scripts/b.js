db1e851b35ccd0b8b999172c0bcaa9d8497a5bc7
andreyyk6 <martins.andrey@escola.pr.gov.br>
2024-02-27 11:06:55 -0300

    projeto back end

---
+++ scripts/b.js
@@ -0,0 +1,35 @@
+//PONTO FLUTUANTE
+const idade = 28;
+const pi = 3.14;
+//INFINITY
+var a = 10
+var b = 0
+console.log(a/b)
+//NaN
+var a = 0
+var b = 0
+console.log(a/b)
+//STRING
+const cidade = "Guarapuava";
+const input = "Guarapuava";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); 
+// booleanos
+
+const numbe1 = 93;
+const numbe2 = 674;
+let cadastroativado;
+
+console.log(numbe1 === numbe2);
+
+//null
+
+let Input = null;
+
+if(input === null) {
+    console.log("não há informação")
+} else {
+    console.log(input);
+}
